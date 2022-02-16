f0a101dd824eacff16cd5770917026e41f18d0b1
Majo <champiis49@gmail.com>
2022-02-16 15:38:25 -0600

    cart fixed

--- src/redux/reducers/handleCart.js
+++ src/redux/reducers/handleCart.js
@@ -1,30 +1,34 @@
-const initialState = [
-
-];
+const initialState = []
 
 const handleCart = (state = initialState, action) => {
-    const product = action.payload;
-    switch (action.type) {
-        case "ADDITEM":
-            const exist = state.find((p) => p.id === product.id);
-            if (exist) {
-                return state.map((p, index) => p.id === product.id ? { ...p, qty: p.qty + 1 } : p);
-            } else {
-                const product = action.payload;
-                return [ ...state, { ...product, qty: 1 } ];
-            }
+  const product = action.payload
+  switch (action.type) {
+    case 'ADDITEM':
+      const exist = state.find((p) => p.id === product.id)
+      if (exist) {
+        return state.map((p, index) =>
+          p.id === product.id
+            ? { ...p, qty: p.qty + 1, total: p.price * p.qty }
+            : p,
+        )
+      } else {
+        const product = action.payload
+        return [...state, { ...product, qty: 1, total: product.price }]
+      }
 
-        case "DELITEM":
-            const exist1 = state.find((p) => p.id === product.id);
-            if (exist1.qty === 1) {
-                return state.filter((p) => p.id !== product.id);
-            } else {
-                return state.map((p) => p.id === product.id ? { ...p, qty: p.qty - 1 } : p);
-            }
+    case 'DELITEM':
+      const exist1 = state.find((p) => p.id === product.id)
+      if (exist1.qty === 1) {
+        return state.filter((p) => p.id !== product.id)
+      } else {
+        return state.map((p) =>
+          p.id === product.id ? { ...p, qty: p.qty - 1 } : p,
+        )
+      }
 
-        default:
-            return state;
-    }
-};
+    default:
+      return state
+  }
+}
 
-export default handleCart;
+export default handleCart
